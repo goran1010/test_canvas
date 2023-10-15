52580eb8f8056178ea2881d710f9493f737ef9f3
Goran Jovic <goran1010jovic@gmail.com>
2023-10-15 22:32:43 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const canvas = document.querySelector(`.canvas`);
+
+const setSize = document.querySelector(`.sizeOfCanvas>input`);
+const setCells = document.querySelector(`.numberOfCells>input`);
+
+let size;
+let cells;
+
+function drawCanvas() {
+  size = document.querySelector(`.sizeOfCanvas>input`).value;
+  canvas.style.width = `${size}px`;
+  canvas.style.height = `${size}px`;
+}
+
+function createCanvas() {
+  drawCanvas();
+  if (document.querySelector(`.numberOfCells>input`).value) {
+    drawCells();
+  }
+}
+
+function drawCells() {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+  let cells = document.querySelector(`.numberOfCells>input`).value;
+  for (let i = 0; i < cells; i++) {
+    let row = document.createElement(`div`);
+    canvas.appendChild(row);
+    for (let j = 0; j < cells; j++) {
+      let singleCell = document.createElement(`div`);
+      singleCell.style.width = `${size / cells}px`;
+      singleCell.style.height = `${size / cells}px`;
+
+      singleCell.addEventListener(`mouseover`, () => {
+        singleCell.classList.toggle(`showCell`);
+      });
+
+      row.appendChild(singleCell);
+    }
+  }
+}
+
+setSize.addEventListener(`keyup`, createCanvas);
+setCells.addEventListener(`keyup`, drawCells);
+
+createCanvas();
